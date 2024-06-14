f44537d140f4d336d8e2949958d91bafc4fa38d7
Gorthdzx <krislaksana123@gmail.com>
2024-06-14 17:53:20 +0700

    feat : data class

---
+++ app/src/main/java/app/dito/scrambleun/data/WordData.kt
@@ -0,0 +1,14 @@
+package app.dito.scrambleun.data
+
+const val MAX_NO_WORDS = 10
+const val SCORE_INCREASES = 20
+
+fun scrambleListWords(): Set<String> {
+    return setOf(
+        "aiti",
+        "makan",
+        "ngoding",
+        "dingin",
+        "lurah"
+    )
+}
